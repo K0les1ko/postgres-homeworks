2a43c6fdd56243b62d4b59603503f66dc31f06d7
Nikolay Yaroshenko <nikolayyaroshenko@MacBook-Pro-Nikolay.local>
2023-12-17 23:05:57 +0300

    no coonection

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,34 @@
 -- SQL-команды для создания таблиц
+
+-- Создание таблицы employees
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    job_title VARCHAR(100),
+    department VARCHAR(50),
+    hire_date DATE,
+    salary DECIMAL(10, 2)
+);
+
+-- Создание таблицы customers
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone_number VARCHAR(20),
+    address VARCHAR(255)
+);
+
+-- Создание таблицы orders
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customers(customer_id),
+    employee_id INTEGER REFERENCES employees(employee_id),
+    order_date DATE,
+    ship_date DATE,
+    product_name VARCHAR(100),
+    quantity INTEGER,
+    total_price DECIMAL(10, 2)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,54 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+
+try:
+
+    conn = psycopg2.connect(
+        host="localhost",
+        database="north",
+        user="postgres",
+        password="12345"
+    )
+
+
+    with conn.cursor() as cursor:
+
+
+        def fill_employees_table():
+            with open("north_data/employees.csv", "r") as file:
+                next(file)
+                for line in file:
+                    data = line.strip().split(",")
+                    cursor.execute("INSERT INTO employees (first_name, last_name, job_title, department, hire_date, salary) VALUES (%s, %s, %s, %s, %s, %s)",
+                                   (data[0], data[1], data[2], data[3], data[4], data[5]))
+
+        def fill_customers_table():
+            with open("north_data/customers.csv", "r") as file:
+                next(file)
+                for line in file:
+                    data = line.strip().split(",")
+                    cursor.execute("INSERT INTO customers (first_name, last_name, email, phone_number, address) VALUES (%s, %s, %s, %s, %s)",
+                                   (data[0], data[1], data[2], data[3], data[4]))
+
+        def fill_orders_table():
+            with open("north_data/orders.csv", "r") as file:
+                next(file)
+                for line in file:
+                    data = line.strip().split(",")
+                    cursor.execute("INSERT INTO orders (customer_id, employee_id, order_date, ship_date, product_name, quantity, total_price) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                                   (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+
+
+        fill_employees_table()
+        fill_customers_table()
+        fill_orders_table()
+
+
+    conn.commit()
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    if conn:
+        conn.close()
