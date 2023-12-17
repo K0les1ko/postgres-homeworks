9017db724180a42a9a2a49e612abb03007f07625
Nikolay Yaroshenko <nikolayyaroshenko@MacBook-Pro-Nikolay.local>
2023-12-17 23:10:16 +0300

    no connection

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,54 +1,53 @@
+"""Скрипт для заполнения данными таблиц в БД Postgres."""
 import psycopg2
 import os
 
-try:
-
-    conn = psycopg2.connect(
-        host="localhost",
-        database="north",
-        user="postgres",
-        password="12345"
-    )
 
 
-    with conn.cursor() as cursor:
+# Подключение к базе данных
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
 
+# Создание курсора для выполнения SQL-запросов
+cursor = conn.cursor()
 
-        def fill_employees_table():
-            with open("north_data/employees.csv", "r") as file:
-                next(file)
-                for line in file:
-                    data = line.strip().split(",")
-                    cursor.execute("INSERT INTO employees (first_name, last_name, job_title, department, hire_date, salary) VALUES (%s, %s, %s, %s, %s, %s)",
-                                   (data[0], data[1], data[2], data[3], data[4], data[5]))
+# Заполнение таблиц данными
+def fill_employees_table():
+    with open("north_data/employees.csv", "r") as file:
+        next(file)  # Пропускаем заголовок
+        for line in file:
+            data = line.strip().split(",")
+            cursor.execute("INSERT INTO employees (first_name, last_name, job_title, department, hire_date, salary) VALUES (%s, %s, %s, %s, %s, %s)",
+                           (data[0], data[1], data[2], data[3], data[4], data[5]))
 
-        def fill_customers_table():
-            with open("north_data/customers.csv", "r") as file:
-                next(file)
-                for line in file:
-                    data = line.strip().split(",")
-                    cursor.execute("INSERT INTO customers (first_name, last_name, email, phone_number, address) VALUES (%s, %s, %s, %s, %s)",
-                                   (data[0], data[1], data[2], data[3], data[4]))
+def fill_customers_table():
+    with open("north_data/customers.csv", "r") as file:
+        next(file)  # Пропускаем заголовок
+        for line in file:
+            data = line.strip().split(",")
+            cursor.execute("INSERT INTO customers (first_name, last_name, email, phone_number, address) VALUES (%s, %s, %s, %s, %s)",
+                           (data[0], data[1], data[2], data[3], data[4]))
 
-        def fill_orders_table():
-            with open("north_data/orders.csv", "r") as file:
-                next(file)
-                for line in file:
-                    data = line.strip().split(",")
-                    cursor.execute("INSERT INTO orders (customer_id, employee_id, order_date, ship_date, product_name, quantity, total_price) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                                   (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+def fill_orders_table():
+    with open("north_data/orders.csv", "r") as file:
+        next(file)  # Пропускаем заголовок
+        for line in file:
+            data = line.strip().split(",")
+            cursor.execute("INSERT INTO orders (customer_id, employee_id, order_date, ship_date, product_name, quantity, total_price) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                           (data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
 
+# Заполнение таблиц данными
+fill_employees_table()
+fill_customers_table()
+fill_orders_table()
 
-        fill_employees_table()
-        fill_customers_table()
-        fill_orders_table()
+# Применение изменений
+conn.commit()
 
-
-    conn.commit()
-
-except Exception as e:
-    print(f"Error: {e}")
-
-finally:
-    if conn:
-        conn.close()
+# Закрытие соединения с базой данных
+cursor.close()
+conn.close()
